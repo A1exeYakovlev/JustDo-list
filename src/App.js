6301cb119eb9a303a1139fc96819ea6bb5aead0f
A1exeYakovlev <118552675+A1exeYakovlev@users.noreply.github.com>
2024-08-23 11:36:47 +0300

    Fix: activate "all" filter on startup
    This was implemented by navigating the starter
    route to the "all" parameter.

--- src/App.js
+++ src/App.js
@@ -2,29 +2,28 @@ import { useState } from "react";
 import TaskInput from "./components/TaskInput";
 import TaskList from "./components/TaskList";
 import Top from "./components/Top";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 export default function App() {
   const [newTask, setNewTask] = useState("");
   const [taskList, setTaskList] = useState([]);
 
   return (
-    <BrowserRouter>
-      <div className="app">
-        <h1 className="heading">
-          Just<span>Do</span> List
-        </h1>
+    <div className="app">
+      <h1 className="heading">
+        Just<span>Do</span> List
+      </h1>
+      <BrowserRouter>
         <div className="app__wrap">
           <Top taskList={taskList} />
           <TaskInput newTask={newTask} setNewTask={setNewTask} setTaskList={setTaskList} />
           <Routes>
-            <Route index element={<TaskList taskList={taskList} setTaskList={setTaskList} />} />
+            <Route index element={<Navigate to="all" />} />
             <Route path="/:filterBy" element={<TaskList taskList={taskList} setTaskList={setTaskList} />} />
           </Routes>
-
         </div>
-      </div>
-    </BrowserRouter>
+      </BrowserRouter>
+    </div>
   );
 }
 
