9b0b7a1904ff47e6fd56827f03d7900bd51e553f
A1exeYakovlev <118552675+A1exeYakovlev@users.noreply.github.com>
2024-08-23 13:29:24 +0300

    Fix: enable page reloading on gh-pages
    Replace BrowserRouter with HashRouter due
    to gh-pages' inability to directly access the SPA
    using URL parameters.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import TaskInput from "./components/TaskInput";
 import TaskList from "./components/TaskList";
 import Top from "./components/Top";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 
 export default function App() {
   const [newTask, setNewTask] = useState("");
@@ -13,7 +13,7 @@ export default function App() {
       <h1 className="heading">
         Just<span>Do</span> List
       </h1>
-      <BrowserRouter basename="/JustDo-list">
+      <HashRouter>
         <div className="app__wrap">
           <Top taskList={taskList} />
           <TaskInput newTask={newTask} setNewTask={setNewTask} setTaskList={setTaskList} />
@@ -22,7 +22,7 @@ export default function App() {
             <Route path="/:filterBy" element={<TaskList taskList={taskList} setTaskList={setTaskList} />} />
           </Routes>
         </div>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
